58cbce71ab8fc15ba67b66b727468f1724652e80
juliabarrosgomes <julia.barros.gomes@escola.pr.gov.br>
2026-07-01 07:54:29 -0300

    refiz a remoçao das bordas agr ta certo

--- 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
+++ 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
@@ -25,7 +25,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (100, 100, largura - 100, altura - 100) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (147, 148, largura - 125, altura - 417) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
